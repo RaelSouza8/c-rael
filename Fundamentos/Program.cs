20b68bcab976dafd2a924385b9e7c3dd45c74cb5
raelsouza <raelsouzadasilva444@gmail.com>
2025-10-01 21:54:35 -0300

    variaveis e I/O

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+#region meu primeiro programa
+
+/* Console.WriteLine("Hello, World!"); */
+#endregion
+
+#region Tipos de variaveis
+
+
+/* 
+string nome = "claudio";
+Console.WriteLine($"Seu nome é {nome}");
+
+int idade;
+int idade02 = 30;
+double altura = 1.75;
+double altura02 = 1;
+
+bool estudante;
+bool estudante02 = true;
+bool estudante03 = false; */
+#endregion
+
+#region entrada e saida de dados
+
+Console.WriteLine("digite seu nome");
+string nomeuser = Console.ReadLine()!;
+Console.Write($"seu nome é {nomeuser}")
+
+#endregion
